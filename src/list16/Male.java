cabb6821ca2be38eebad2d754450d519d9e862e4
Alexander Holmstrom <alexholmstrom.se>
2021-03-10 19:17:50 +0100

    Created classes Male and Person

---
+++ src/list16/Male.java
@@ -0,0 +1,14 @@
+package list16;
+
+public class Male extends Person {
+	private static final String CLASS_NAME = "Male";
+
+	public Male(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void printClassName() {
+		System.out.println("The person "+ name + " belongs to class: "+ CLASS_NAME);
+	}
+}
---
+++ src/list16/Person.java
@@ -0,0 +1,9 @@
+package list16;
+
+public abstract class Person {
+
+	protected String name;
+	protected int age;
+	
+	public abstract void printClassName();
+}
